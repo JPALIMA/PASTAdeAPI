1d1025a951f45aef2aa45659cefbe0846d20362b
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-09-04 20:58:06 -0300

    gerando pasta API4

---
+++ api_4/trabalho.rb
@@ -0,0 +1,9 @@
+# Para verificar os arquivos com conflito e suas diferenças
+git status
+
+# Edite os arquivos com conflito para resolver as diferenças
+# Depois de resolver, use o seguinte comando para marcar o arquivo como resolvido
+git add CAMINHO_DO_ARQUIVO
+
+# Continue com a fusão (merge)
+git commit -m "Resolve merge conflicts"
