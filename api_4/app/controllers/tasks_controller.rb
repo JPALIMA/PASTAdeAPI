7489e55828959729731f985a88c38bee2ae944ae
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-09-04 21:30:59 -0300

    projeto de uma API simples de tarefas (To-Do List), que suporta operações CRUD (Create, Read, Update, Delete)

--- api_4/app/controllers/tasks_controller.rb
+++ api_4/app/controllers/tasks_controller.rb
@@ -1,46 +1,49 @@
 class TasksController < ApplicationController
+     before_action :set_task, only: [:show, :update, :destroy]
+   
+     # GET /tasks
      def index
-          tasks = Task.all
-          render json: tasks
+       @tasks = Task.all
+       render json: @tasks
      end
-
+   
+     # GET /tasks/1
      def show
-          task = Task.find(params[:id])
-          render json: task
+       render json: @task
      end
-
+   
+     # POST /tasks
      def create
-          task = Task.find(params[:id])
-          render json: task
+       @task = Task.new(task_params)
+   
+       if @task.save
+         render json: @task, status: :created, location: @task
+       else
+         render json: @task.errors, status: :unprocessable_entity
+       end
      end
-
-     def create
-          task = Task.new(task_params)
-          if task.save
-               render json: task, status: :created
-          else
-               render json: task.errors, status: :unprocessable_entity
-          end
-     end
-
+   
+     # PATCH/PUT /tasks/1
      def update
-          task = Task.find(params[:id])
-          if task.update(task_params)
-               render json: task
-          else
-               render json: task.errors, status: :unprocessable_entity
-          end
+       if @task.update(task_params)
+         render json: @task
+       else
+         render json: @task.errors, status: :unprocessable_entity
+       end
      end
-
+   
+     # DELETE /tasks/1
      def destroy
-          task = Task.find(params[:id])
-          task.destroy
-          head :no_content
+       @task.destroy
      end
-
+   
      private
+   
+     def set_task
+          @task = Task.find(params[:id])
+     end
 
      def task_params
-          params.require(:task).permit(:title, :completed)
+          params.require(:task).permit(:title, :description, :completed)
      end
 end
