947002c25ddaf236a862c6fef55b169368c9bae6
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-09-15 19:51:00 -0300

    Uma API que permite a conversão de valores entre diferentes moedas.

---
+++ API_6_Conversão_Moeda/currency_converter.rb
@@ -0,0 +1,24 @@
+#gem necessária: 'exchange_rate' para obter taxas do câmbio atualizadas
+require 'sinatra'
+require 'exchange_rate'
+
+#definindo as moedas suportadas e suas taxas de câmbio
+EXCHANGE_RATES = {
+     'USD' => 1.0,
+     'EUR' => 0.85,
+     'GBR' => 0.75
+}
+
+get '/convert' do
+     from_currency = params['from']
+     to_currency = params['to']
+     amount = params['amount'].to_f
+
+     if EXCHANGE_RATES.key?(from_currency) && EXCHANGE_RATES.key?(to_currency)
+          converted_amount = amount * (EXCHANGE_RATES[to_currency] / EXCHANGE_RATES[from_currency])
+          {amount: converted_amount, currency: to_currency}.to_json
+     else
+          status 400
+          'Moeda nao suportada'
+     end
+end
