bedb22a304447c5cccc366c9e7bf40d0aaec3974
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-09-20 22:17:47 -0300

    exemplo de uma API de conversão de moeda, configurando e definindo as moedas suportadas e suas taxas de câmbio.

--- API_6_Conversão_Moeda/currency_converter.rb
+++ API_6_Conversão_Moeda/currency_converter.rb
@@ -1,20 +1,20 @@
-#API - definindo as moedas suportadas e suas taxas de câmbio
+#API - Definindo as moedas suportadas e suas taxas de câmbio
 EXCHANGE_RATES = {
      'USD' => 1.0,
      'EUR' => 0.85,
      'GBR' => 0.75
 }
 
-get '/convert' do
-     from_currency = params['from']
+get '/convert' do 
+     from_currecy = params['from']
      to_currency = params['to']
      amount = params['amount'].to_f
 
      if EXCHANGE_RATES.key?(from_currency) && EXCHANGE_RATES.key?(to_currency)
           converted_amount = amount * (EXCHANGE_RATES[to_currency] / EXCHANGE_RATES[from_currency])
-          {amount: coverted_amount, currency: to_currency}.to_json
+          {amount: converted_amount, currency: to_currency}.to_json
      else
           status 400
-          'Moeda nao suporta'
+          'Moeda nao suportada'
      end
 end
